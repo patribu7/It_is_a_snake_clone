1112fe726cc68437b9af38c402fe1c0632c282b4
Patrizia Busatto <patribu7@gmail.com>
2024-08-05 15:19:35 +0200

    Prepare idea for map13
    And fixed a mistake with map12

--- scene/maps/script/map13.gd
+++ scene/maps/script/map13.gd
@@ -11,5 +11,20 @@ func _ready():
 		snake.get_node("Player").snake_eat.connect(_on_snake_eat)
 		snake.get_node("Player").snake_be_defeat.connect(_on_snake_be_defeat)
 		snake.get_node("Player").snake_win.connect(_on_snake_win)
-		
-		$Snake/Timer.set_wait_time(start_timeout)
+
+
+func _on_timer_timeout():
+	$Background.position.y += 50
+	$Apples.position.y += 50
+	
+	for apple in $Apples.get_children():
+		if apple.global_position.y == 200:
+			fall(apple)
+
+
+func fall(obj):
+	while obj.global_position.y <= 700:
+		obj.global_position.y += 10
+		await Global.wait(0.1)
+
+	obj.global_position.y = 700
